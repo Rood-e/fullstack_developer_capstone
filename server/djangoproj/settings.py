d6485b966aaf34a4b8418846d5c8e979819fc11b
Rood-e <rudymartucci@gmail.com>
2025-11-15 07:01:42 -0500

    lint settings pt2

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0"
+SECRET_KEY = ("django-insecure-ccow$tz_=9%dxu4(0%"
+    "^(z%nx32#s@(zt9$ih@)5l54yny)wm-0")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -104,16 +105,28 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         # E231: Aggiunto spazio dopo la virgola in tutte le righe
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation."
+            "UserAttributeSimilarityValidator"
+            ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation."
+            "MinimumLengthValidator"
+        ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation."
+            "CommonPasswordValidator"
+        ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation."
+            "NumericPasswordValidator"
+        ),
     },
 ]
 
@@ -151,4 +164,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "frontend/build"),
     os.path.join(BASE_DIR, "frontend/build/static"),
 ]
-# W391: L'ultima riga vuota alla fine del file è stata rimossa
